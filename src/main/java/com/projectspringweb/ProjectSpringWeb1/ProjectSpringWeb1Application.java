b614a404bca122e18e2df58e434dfab42e9c9c3c
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2024-11-05 12:17:34 -0300

    Initial Commit

---
+++ src/main/java/com/projectspringweb/ProjectSpringWeb1/ProjectSpringWeb1Application.java
@@ -0,0 +1,19 @@
+package com.projectspringweb.ProjectSpringWeb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringWeb1Application.class, args);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/test/java/com/projectspringweb/ProjectSpringWeb1/ProjectSpringWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectspringweb.ProjectSpringWeb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
